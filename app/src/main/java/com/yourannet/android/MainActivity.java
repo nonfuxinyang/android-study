7a8e68fce3c87bea5eb3b5eaa53b3685a28e181d
nonfuxinyang <nonfuxinyang@gmail.com>
2017-08-21 09:53:57 +0800

    添加了版本号

--- app/src/main/java/com/yourannet/android/MainActivity.java
+++ app/src/main/java/com/yourannet/android/MainActivity.java
@@ -1,13 +1,28 @@
 package com.yourannet.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_hello)
+    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_hello)
+    public void onViewClicked() {
+        Toast.makeText(this,"Hello world !",Toast.LENGTH_LONG).show();
     }
 }
